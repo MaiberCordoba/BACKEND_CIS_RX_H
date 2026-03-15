eda37a6e5cb7e8f57679a664de9c61ffa21e8e89
maiberCordoba <cordobamaiber@gmail.com>
2026-03-15 11:14:32 -0500

    creacion modelo de usuarios

--- CIS_RX_API/apps/users/models.py
+++ CIS_RX_API/apps/users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Usuario(AbstractUser):
+    ROLES = (
+        ('Facturador', 'Facturador'),
+        ('Jefe', 'Jefe/Responsable'),
+    )
+    rol = models.CharField(max_length=20, choices=ROLES, default='Facturador')
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(unique=True)
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.rol})"
