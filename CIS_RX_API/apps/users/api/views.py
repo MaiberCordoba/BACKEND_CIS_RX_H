4828530d7fc553d08b76d17f955f66114177e211
maiberCordoba <cordobamaiber@gmail.com>
2026-03-15 11:24:28 -0500

    creando vista para el crud de usuarios

---
+++ CIS_RX_API/apps/users/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from ..models import Usuario
+from .serializers import UsuarioSerializer, UsuarioCreateSerializer
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    
+    # funcion para usar el serializador que encripta la contraseña al crear usuarios
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UsuarioCreateSerializer
+        return UsuarioSerializer
+
+    permission_classes = [IsAuthenticated]
